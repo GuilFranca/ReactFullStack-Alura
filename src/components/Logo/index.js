29cc30c030912d43c28be29c297e7867de351118
Guilherme França <guilfranca27@gmail.com>
2026-09-03 12:01:44 -0300

    Aula 2 - 6. Estilizando Finalizada

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -4,7 +4,11 @@ import './Logo.css';
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='Logo Alura Books' />
+            <img
+                src={logo}
+                alt='Logo Alura Books'
+                className="logo-img"
+            />
             <p><strong>Alura</strong>Books</p>
         </div>
     )
