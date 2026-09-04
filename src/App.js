4046935ba136b26c5c751c73fa4e30aa15ba7263
Guilherme França <guilfranca27@gmail.com>
2026-09-03 23:31:45 -0300

    Aula 2 - 8. Usando Loops Finalizada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 import Logo from "./components/Logo/index.js";
+import perfil from './images/perfil.svg';
+import sacola from './images/sacola.svg';
+
+const textoOpcoes = [
+  "CATEGORIAS",
+  "MINHA ESTANTE",
+  "FAVORITOS"
+];
+const icones = [
+  perfil,
+  sacola
+]
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo />
+        <ul className='opcoes'>
+          {textoOpcoes.map((texto) => (
+            <li className='opcao'><p>{texto}</p></li>
+          ))}
+        </ul>
+        <ul className='icones'>
+            {icones.map((icone) => (
+              <li className='icone'><img src={icone} /></li>
+            ))}
+        </ul>
       </header>
     </div>
   );
