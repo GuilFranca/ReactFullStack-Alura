8fbbb7e1c7841af9d96a0f39e173e75d46343d0d
Guilherme França <guilfranca27@gmail.com>
2026-09-09 23:46:04 -0300

    Aula 5 - 2. Componente livros novos Finalizada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header/index.js';
 import styled from 'styled-components';
 import Pesquisa from './components/Pesquisa/index.js';
+import UltimosLancamentos from './components/UltimosLancamentos/index.js';
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(90deg,#002F52 35%,#326589);
+  /* background-image: linear-gradient(90deg,#002F52 35%,#326589); */
 `;
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <AppContainer>
       <Header />
       <Pesquisa />
+      <UltimosLancamentos />
     </AppContainer>
   );
 }
